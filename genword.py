52eb33d01bb4e0d2ab18d6a7a0b29bbda2a439ae
zedl3all <sanguanwongtanapat@gmail.com>
2023-09-04 20:35:47 +0700

    first commit

---
+++ genword.py
@@ -0,0 +1,20 @@
+"""generate word"""
+import json
+from bardapi import Bard
+
+def opentoken():
+    """GetToken"""
+    with open("token.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data["token"]
+
+def generate():
+    """generate"""
+    opentoken()
+    bard = Bard(token=opentoken())
+    output = bard.get_answer("สุ่มประโยค 1 ประโยคสำหรับคุยกับเพื่อนเป็นภาษาไทย")
+    outputcontent = output["content"]
+    if '"' in outputcontent:
+        extracted_word = outputcontent.split('"')[1]
+        #print(extracted_word)
+        return extracted_word

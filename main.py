69ea9937248922f311a9e21c0d80080881cd1d2d
zedl3all <sanguanwongtanapat@gmail.com>
2023-09-05 22:30:29 +0700

    change send message to DM

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ import genword
 
 def gettoken():
     """GetToken"""
-    with open("token.json", "r", encoding="utf-8") as file:
+    with open('Token.json', "r", encoding="utf-8") as file:
         data = json.load(file)
     return data["tokenBotdiscord"]
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
-canloop = False
 channel_id = 1148255715937505360
 
 @client.event
@@ -23,9 +22,20 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     channel = client.get_channel(channel_id)
     await channel.send("Bot is Online")
-    while True:
-        await channel.send(genword.generate())
-        time.sleep(60)
-
+    #while True:
+    #    await channel.send(genword.generate())
+    #    time.sleep(60)
+@client.event
+async def on_message(message):
+    """send message"""
+    if message.author == client.user:
+        return
+    if message.content == '!TEST':
+        try:
+            randomword = genword.generate()
+            time.sleep(5)
+            await message.author.send(randomword)
+        except: # Ignoring exception in on_message
+            await message.author.send("กรุณาลองใหม่")
 
 client.run(gettoken())
